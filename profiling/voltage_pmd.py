9d345f75dd00b68b6c85e01ace2efef1c49f82e8
ian kinsella <ikinsella@wisc.edu>
2018-04-21 18:00:49 -0400

    updating profiling script to generate multithreaded results

--- profiling/voltage_pmd.py
+++ profiling/voltage_pmd.py
@@ -1,18 +1,17 @@
 import numpy as np
-from trefide.pmd import serial_batch_pmd, parallel_batch_pmd
+from trefide.pmd import batch_decompose
 
 if __name__ == "__main__":
     X = np.load("/home/ian/devel/trefide/data/prepared_sampleMovie.npy")
-    X = np.tile(X, (5, 2))
+    X = np.tile(X, (10, 1))
     d1, d2, T = X.shape
 
     K = 20
     maxiter = 50
     tol = 5e-3
-    bheight = 20
-    bwidth = 100
+    bheight = 40
+    bwidth = 40
     spatial_cutoff = (bheight*bwidth / ((bheight*(bwidth-1) + bwidth*(bheight-1))))
     w = .0025
     
-    # U, V, K, indices = serial_batch_pmd(d1, d2, T, X, bheight, bwidth, w, spatial_cutoff, K, maxiter, tol)
-    U, V, K, indices = parallel_batch_pmd(d1, d2, T, X, bheight, bwidth, w, spatial_cutoff, K, maxiter, tol)
+    U, V, K, indices = batch_decompose(d1, d2, T, X, bheight, bwidth, w, spatial_cutoff, K, maxiter, tol)
