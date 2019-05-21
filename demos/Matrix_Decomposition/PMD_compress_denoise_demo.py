8aa7db98bbcc36dd2eeee80c70e2be53a144e2f5
Jian Wang <jw3132@columbia.edu>
2019-05-21 10:40:10 -0400

    added PMF demo script

---
+++ demos/Matrix_Decomposition/PMD_compress_denoise_demo.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# General Dependencies
+import os
+import numpy as np
+
+# Preprocessing Dependencies
+from trefide.utils import psd_noise_estimate
+
+# PMD Model Dependencies
+from trefide.pmd import batch_decompose,                        batch_recompose,                        overlapping_batch_decompose,                        overlapping_batch_recompose,                        determine_thresholds
+from trefide.reformat import overlapping_component_reformat
+
+# Plotting & Video Rendering Dependencies
+import matplotlib.pyplot as plt
+from trefide.plot import pixelwise_ranks
+from trefide.extras.util_plot import comparison_plot
+from trefide.video import play_cv2
+
+# Set Demo Dataset Location
+ext = os.path.join("..", "data")
+filename = os.path.join(ext, "demoMovie.npy")
+
+# get_ipython().run_line_magic('load_ext', 'autoreload')
+# get_ipython().run_line_magic('autoreload', '2')
+
+
+# # Load Preprocessed Data
+
+# In[4]:
+
+
+mov = np.load(filename)
+fov_height, fov_width, num_frames = mov.shape
+
+
+# # Set Params
+
+# In[5]:
+
+
+# Generous maximum of rank 50 blocks (safeguard to terminate early if this is hit)
+max_components = 50
+
+# Enable Decimation 
+max_iters_main = 10
+max_iters_init = 40
+d_sub=2
+t_sub=2
+
+# Defaults
+consec_failures = 3
+tol = 5e-3
+
+# Set Blocksize Parameters
+block_height = 40
+block_width = 40
+overlapping = True
+
+
+# # Compress Video
+
+# ## Simulate Critical Region Using Noise
+
+# In[6]:
+
+
+spatial_thresh, temporal_thresh = determine_thresholds((fov_height, fov_width, num_frames),
+                                                       (block_height, block_width),
+                                                       consec_failures, max_iters_main, 
+                                                       max_iters_init, tol, 
+                                                       d_sub, t_sub, 5, True)
+
+
+# ## Decompose Each Block Into Spatial & Temporal Components
+
+# In[7]:
+
+
+if not overlapping:    # Blockwise Parallel, Single Tiling
+    spatial_components,    temporal_components,    block_ranks,    block_indices = batch_decompose(fov_height, fov_width, num_frames,
+                                    mov, block_height, block_width,
+                                    max_components, consec_failures,
+                                    max_iters_main, max_iters_init, tol,
+                                    d_sub=d_sub, t_sub=t_sub)
+else:    # Blockwise Parallel, 4x Overlapping Tiling
+    spatial_components,    temporal_components,    block_ranks,    block_indices,    block_weights = overlapping_batch_decompose(fov_height, fov_width, num_frames,
+                                                mov, block_height, block_width,
+                                                spatial_thresh, temporal_thresh,
+                                                max_components, consec_failures,
+                                                max_iters_main, max_iters_init, tol,
+                                                d_sub=d_sub, t_sub=t_sub)
+
+
+# # Reconstruct Denoised Video
+
+# In[8]:
+
+
+if not overlapping:  # Single Tiling (No need for reqweighting)
+    mov_denoised = np.asarray(batch_recompose(spatial_components,
+                                              temporal_components,
+                                              block_ranks,
+                                              block_indices))
+else:   # Overlapping Tilings With Reweighting
+    mov_denoised = np.asarray(overlapping_batch_recompose(fov_height, fov_width, num_frames,
+                                                          block_height, block_width,
+                                                          spatial_components,
+                                                          temporal_components,
+                                                          block_ranks,
+                                                          block_indices,
+                                                          block_weights)) 
+
+
+# # Produce Diagnostics
+
+# ### Single Tiling Pixel-Wise Ranks
+
+# In[9]:
+
+
+if overlapping:
+    pixelwise_ranks(block_ranks['no_skew']['full'], fov_height, fov_width, num_frames, block_height, block_width)
+else:
+    pixelwise_ranks(block_ranks, fov_height, fov_width, num_frames, block_height, block_width)
+
+
+# ### Correlation Images
+
+# In[10]:
+
+
+comparison_plot([mov, mov_denoised + np.random.randn(np.prod(mov.shape)).reshape(mov.shape)*.01],
+                plot_orientation="horizontal")
+
+
+# ## Render Videos & Residual
+
+# In[11]:
+
+
+play_cv2(np.vstack([mov, mov_denoised, mov-mov_denoised]), magnification=2)
+
+
+# # Save Results
+
+# In[12]:
+
+
+U, V = overlapping_component_reformat(fov_height, fov_width, num_frames,
+                                      block_height, block_width,
+                                      spatial_components,
+                                      temporal_components,
+                                      block_ranks,
+                                      block_indices,
+                                      block_weights)
+
+
+# In[13]:
+
+
+np.savez(os.path.join(ext, "demo_results.npz"), U, V)
+
+
+#    
