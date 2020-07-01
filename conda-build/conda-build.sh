39dd0bbae23d7f13a16d35271e475f7e54fc9696
Ian Kinsella <ikinsella@wisc.edu>
2020-07-01 00:05:08 -0400

    Added new docker image to automate conda-build process.

--- conda-build/conda-build.sh
+++ conda-build/conda-build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-#conda config --set anaconda_upload yes  # automatically upload successful builds
+# Configure Anaconda Client To Upload Successful Builds
+anaconda login
+conda config --set anaconda_upload yes
+
+# Run Builds Across Matrix Specified In Config
 conda-build . -c defaults -c conda-forge --variant-config-file conda_build_config.yaml
