fa0d53c0a43c141d6d1564c060266a8ef41dfe42
Ian Kinsella <ikinsella@wisc.edu>
2020-07-01 20:36:48 -0400

    letting conda-build use intel packages to circumvent unresolvable dependency conflicts.

--- conda-build/conda-build.sh
+++ conda-build/conda-build.sh
@@ -5,4 +5,4 @@ anaconda login
 conda config --set anaconda_upload yes
 
 # Run Builds Across Matrix Specified In Config
-conda-build . -c defaults -c conda-forge --variant-config-file conda_build_config.yaml
+conda-build . -c intel -c defaults -c conda-forge --variant-config-file conda_build_config.yaml
